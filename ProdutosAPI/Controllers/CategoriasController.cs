7bb8e1ed4ca4c4cbda59d29f1250f386a3b14abf
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2026-01-14 10:32:05 -0300

    Implementado lógica de DTO. Atualizado CategoriaController para contemplar os DTOs de forma manual

--- ProdutosAPI/Controllers/CategoriasController.cs
+++ ProdutosAPI/Controllers/CategoriasController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProdutosAPI.Context;
+using ProdutosAPI.DTOs;
 using ProdutosAPI.Filters;
 using ProdutosAPI.Models;
 using ProdutosAPI.Repositories;
@@ -22,52 +23,101 @@ namespace ProdutosAPI.Controllers
 
         [HttpGet]
         [ServiceFilter(typeof(ApiLoggingFilter))] // Applying the logging filter
-        public ActionResult<IEnumerable<Categoria>> GetCategorias()
+        public ActionResult<IEnumerable<CategoriaDTO>> GetCategorias()
         {
             var categorias = _uof.CategoriaRepository.GetAll();
-            return Ok(categorias);
+
+            var categoriasDto = new List<CategoriaDTO>();
+            foreach (var categoria in categorias)
+            {
+                categoriasDto.Add(new CategoriaDTO()
+                {
+                    CategoriaId = categoria.CategoriaId,
+                    Nome = categoria.Nome,
+                    ImagemUrl = categoria.ImagemUrl
+                });
+            }
+
+            return Ok(categoriasDto);
         }
 
         [HttpGet("{id:int}", Name = "ObterCategoria")]
-        public ActionResult<Categoria> GetCategoria(int id)
+        public ActionResult<CategoriaDTO> GetCategoria(int id)
         {
             var categoria = _uof.CategoriaRepository.Get(c => c.CategoriaId == id);
             if(categoria is null)
             {
                 return NotFound("Categoria não encontrada!");
             }
-            return Ok(categoria);
+
+            var categoriaDTO = new CategoriaDTO()
+            {
+                CategoriaId = categoria.CategoriaId,
+                Nome = categoria.Nome,
+                ImagemUrl = categoria.ImagemUrl
+            };
+
+            return Ok(categoriaDTO);
         }
 
         [HttpPost]
-        public ActionResult<Categoria> PostCategoria(Categoria categoria)
+        public ActionResult<CategoriaDTO> PostCategoria(CategoriaDTO categoriaDto)
         {
-            if(categoria is null)
+            if(categoriaDto is null)
             {
                 return BadRequest("Categoria inválida");
             }
 
+            var categoria = new Categoria()
+            {
+                CategoriaId = categoriaDto.CategoriaId,
+                Nome = categoriaDto.Nome,
+                ImagemUrl = categoriaDto.ImagemUrl
+            };
+
             var categoriaCriada = _uof.CategoriaRepository.Create(categoria);
             _uof.Commit();
 
-            return new CreatedAtRouteResult("ObterCategoria", new { id = categoriaCriada.CategoriaId }, categoriaCriada);
+            var novaCategoriaDTO = new CategoriaDTO()
+            {
+                CategoriaId = categoriaCriada.CategoriaId,
+                Nome = categoriaCriada.Nome,
+                ImagemUrl = categoriaCriada.ImagemUrl
+            };
+
+            return new CreatedAtRouteResult("ObterCategoria", new { id = novaCategoriaDTO.CategoriaId }, novaCategoriaDTO);
         }
 
         [HttpPut("{id:int}")]
-        public ActionResult<Categoria> PutCategoria(int id, Categoria categoria)
+        public ActionResult<CategoriaDTO> PutCategoria(int id, CategoriaDTO categoriaDto)
         {
-            if(id != categoria.CategoriaId)
+            if(id != categoriaDto.CategoriaId)
             {
                 return BadRequest();
             }
 
-            _uof.CategoriaRepository.Update(categoria);
+            var categoria = new Categoria()
+            {
+                CategoriaId = categoriaDto.CategoriaId,
+                Nome = categoriaDto.Nome,
+                ImagemUrl = categoriaDto.ImagemUrl
+            };
+
+            var categoriaAtualizada = _uof.CategoriaRepository.Update(categoria);
             _uof.Commit();
-            return Ok(categoria);
+
+            var novaCategoriaDTO = new CategoriaDTO()
+            {
+                CategoriaId = categoriaAtualizada.CategoriaId,
+                Nome = categoriaAtualizada.Nome,
+                ImagemUrl = categoriaAtualizada.ImagemUrl
+            };
+
+            return Ok(novaCategoriaDTO);
         }
 
         [HttpDelete("{id:int}")]
-        public ActionResult<Categoria> DeleteCategoria(int id)
+        public ActionResult<CategoriaDTO> DeleteCategoria(int id)
         {
             var categoria = _uof.CategoriaRepository.Get(c => c.CategoriaId == id);
 
@@ -79,7 +129,14 @@ namespace ProdutosAPI.Controllers
             var categoriaExcluida = _uof.CategoriaRepository.Delete(categoria);
             _uof.Commit();
 
-            return Ok(categoria);
+            var novaCategoriaDTO = new CategoriaDTO()
+            {
+                CategoriaId = categoriaExcluida.CategoriaId,
+                Nome = categoriaExcluida.Nome,
+                ImagemUrl = categoriaExcluida.ImagemUrl
+            };
+
+            return Ok(novaCategoriaDTO);
         }
     }
 }
--- ProdutosAPI/obj/Debug/net8.0/ProdutosAPI.AssemblyInfo.cs
+++ ProdutosAPI/obj/Debug/net8.0/ProdutosAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProdutosAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f768149fce1b5626ad9ad76fbfbdb7ca30503c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44821a0ce562d76869d051452166a80ab04a9944")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProdutosAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProdutosAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
